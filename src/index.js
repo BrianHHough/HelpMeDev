6bdc57e7051af9c02525c21ded4764983a4ece1e
Brian H. Hough | brianhhough.eth <56947075+BrianHHough@users.noreply.github.com>
2021-11-08 09:24:42 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import App from './App';
 import { MoralisProvider } from "react-moralis";
 // import reportWebVitals from './reportWebVitals';
 
+const appId = process.env.REACT_APP_MORALIS_APPLICATION_ID;
+const serverUrl = process.env.REACT_APP_MORALIS_SERVER_URL;
+
 ReactDOM.render(
-  // <React.StrictMode>
-  <MoralisProvider appId="cJeGUubKlYjFiAAtM2fKSeGBBgV12uiAycS4De6T" serverUrl="https://zucgwfnukrja.moralishost.com:2053/server">
-    <App />
-  </MoralisProvider>,
-  // </React.StrictMode>,
+  <React.StrictMode>
+    <MoralisProvider appId={appId} serverUrl={serverUrl}>
+      <App />
+    </MoralisProvider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
